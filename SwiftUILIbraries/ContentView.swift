9e6cad32b6e13743bb4e0e0a5c2c8a7c2286e039
Allan Evans <lingoslinger@me.com>
2024-06-29 14:54:13 -0400

    Initial Commit

---
+++ SwiftUILIbraries/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUILIbraries
+//
+//  Created by Allan Evans on 6/29/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUILIbraries/SwiftUILIbrariesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILIbrariesApp.swift
+//  SwiftUILIbraries
+//
+//  Created by Allan Evans on 6/29/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILIbrariesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
